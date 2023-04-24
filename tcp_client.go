8222430778995c2aa4f33e17eb56941b931464ab
vicnorm <victor.normannseth@gmail.com>
2023-04-24 12:50:31 +0000

    client med kryptering

--- tcp_client.go
+++ tcp_client.go
@@ -4,17 +4,20 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/vicnorm/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.5:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,6 +26,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Printf("reply from proxy: %s", string(dekryptertMelding))
 }
